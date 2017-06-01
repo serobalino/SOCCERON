64b8369d4ce597b7291f98b09104d451eb3d9a84
kitty03 <kathe_alba19@outlook.es>
2017-06-01 14:28:36 -0500

    base de datos

--- base de datos/socceron.sql
+++ base de datos/socceron.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     30/5/2017 17:08:39                           */
+/* Created on:     1/6/2017 12:49:36                            */
 /*==============================================================*/
 
 
@@ -19,13 +19,13 @@ drop table if exists partidos;
 /*==============================================================*/
 create table canchas
 (
-   id_can               int not null auto_increment,
-   descripcion_can      varchar(50) not null,
-   sector_can           varchar(30) not null,
-   tipo_can             char(20),
-   latitud_can          decimal(8,2),
-   longitu_can          decimal(8,2),
-   primary key (id_can)
+   id_ca                int not null,
+   descripcion_ca       varchar(50) not null,
+   sector_ca            varchar(30) not null,
+   tipo_ca              char(20),
+   latitud_ca           decimal(8,2),
+   longitu_ca           decimal(8,2),
+   primary key (id_ca)
 );
 
 /*==============================================================*/
@@ -44,7 +44,7 @@ create table conformado
 /*==============================================================*/
 create table equipos
 (
-   id_eq                int not null auto_increment,
+   id_eq                int not null,
    descripcion_eq       varchar(40) not null,
    fecha_eq             timestamp,
    primary key (id_eq)
@@ -55,12 +55,13 @@ create table equipos
 /*==============================================================*/
 create table jugadores
 (
-   id_ju                bigint not null auto_increment,
-   nombre_ju            char(20) not null,
-   contrasena_ju        char(61) not null,
+   id_ju                bigint not null,
+   nombre_ju            char(100) not null,
+   contrasena_ju        char(100),
    correo_ju            varchar(50) not null,
    token_ju             char(100),
-   fb_ju                bool default 0,
+   fb_ju                char(50),
+   estado_ju            bool,
    primary key (id_ju)
 );
 
@@ -69,11 +70,13 @@ create table jugadores
 /*==============================================================*/
 create table partidos
 (
-   id_part              int not null auto_increment,
+   id_pa                int not null,
    id_eq                int not null,
-   id_can               int not null,
-   fecha_part           datetime not null,
-   primary key (id_part)
+   id_ca                int not null,
+   fecha_pa             datetime not null,
+   estado_pa            bool,
+   numerojugadores_part int,
+   primary key (id_pa)
 );
 
 alter table conformado add constraint fk_conformado foreign key (id_ju)
@@ -85,6 +88,6 @@ alter table conformado add constraint fk_conformado2 foreign key (id_eq)
 alter table partidos add constraint fk_juegan foreign key (id_eq)
       references equipos (id_eq) on delete restrict on update restrict;
 
-alter table partidos add constraint fk_utilizan foreign key (id_can)
-      references canchas (id_can) on delete restrict on update restrict;
+alter table partidos add constraint fk_utilizan foreign key (id_ca)
+      references canchas (id_ca) on delete restrict on update restrict;
 
