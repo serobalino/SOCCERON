2d9479f612ad3e3c6cc12659cd8d8e506e320551
kitty03 <kathe_alba19@outlook.es>
2017-06-05 13:23:24 -0500

    autentificacion

--- app/Http/Controllers/Auth/AutenticacionJugador.php
+++ app/Http/Controllers/Auth/AutenticacionJugador.php
@@ -12,18 +12,21 @@ class AutenticacionJugador extends Controller{
     public function __construct(){
         $this->middleware(['guest'],['except' => 'logout']);
     }
+    //hace login
     public function login(Request $request){
-        if(Auth::guard('jug')->attempt(['correo_ju'=>$request->email,'password'=>$request->contrasena],true))
+        if(Auth::guard('jug')->attempt(['correo_ju'=>$request->correo,'password'=>$request->contrasena],true))
           return (['estado'=>true,'mensaje'=>'Credenciales Correctos','vista'=>'inicio','info'=>Auth::user()]);
         else
           return (['estado'=>false,'mensaje'=>'Credenciales Incorrectos','vista'=>'login','url'=>route('login')]);
     }
+    //comprobar que esta logueado
     public function checklog(){
       if (Auth::viaRemember() || Auth::check())
         return (['estado'=>true,'mensaje'=>'Credenciales guardados','vista'=>'inicio','info'=>Auth::user()]);
       else
         return (['estado'=>true,'mensaje'=>'Credenciales no guardados','vista'=>'login','url'=>route('login')]);
     }
+    //para logout
     public function logout(){
       Auth::guard('jug')->logout();
       return (['estado'=>false,'mensaje'=>'Sesion cerrada','vista'=>'login']);
