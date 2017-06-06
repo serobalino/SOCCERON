0da5175954f7f354a82259772d05b9168a81294b
dpancho349 <diegopanchomales@gmail.com>
2017-06-05 20:28:56 -0500

    desacativar y activar cuenta

--- app/Http/Controllers/JugadoresController.php
+++ app/Http/Controllers/JugadoresController.php
@@ -7,11 +7,6 @@ use Illuminate\Support\Facades\Auth;
 use App\Jugador;
 
 class JugadoresController extends Controller{
-
-  public function __construct(){
-    $this->middleware('auth:jug');
-  }
-
   public function index()
   {
     $cualquiera=Jugador::all();
@@ -31,10 +26,36 @@ class JugadoresController extends Controller{
     else
       return "No se guardo";
   }
-  public function desactivarjugador($datos)
-  {
-
+  //select con limit 1 ->first()
+  public function activarjugador(Request $datos){
+    $validacion = Validator::make($datos->all(), [
+        'correo'      => 'required|email',
+    ]);
+    if($validacion->fails()){
+      return (['estado'=>false,'mensaje'=>'Faltan campos o estan incorrectos','vista'=>'login']);
+    }else{
+      $cualquiera = Jugador::where('correo_ju',$datos->correo)->first();//select de id autentificado
+      if (count($cualquiera)) {
+        $cualquiera->estado_ju=true;
+        $cualquiera->save();//guarda o actualizar en la base
+        return (['estado'=>true,'mensaje'=>'Se ha activado la cuenta','vista'=>'login']);
+      }
+      else {
+        return (['estado'=>false,'mensaje'=>'La cuenta no existe','vista'=>'register.submit']);
   }
+}
+}
+public function desactivarjugador(){
+     $cualquiera = Jugador::find(Auth::id());//select de id autentificado
+     $cualquiera->estado_ju=false;
+     if ($cualquiera->save()) {
+       Auth::guard('jug')->logout();//deslogueo
+       return (['estado'=>true,'mensaje'=>'Se ha desactivado la cuenta','vista'=>'login']);
+     }
+    else {
+       return (['estado'=>false,'mensaje'=>'Error','vista'=>'inicio']);
+ }
+ }
   public function modificarjugador(Request $datos)
   {
     $validacion = Validator::make($datos->all(), [
