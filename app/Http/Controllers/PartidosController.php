cba970c9815177bbdd7cc5c6acafb1d03f87b0a5
kitty03 <kathe_alba19@outlook.es>
2017-06-06 22:47:55 -0500

    rutas

--- app/Http/Controllers/PartidosController.php
+++ app/Http/Controllers/PartidosController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Partidos;
+use Validator;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class PartidosController extends Controller
 {
-    //
+    public function crearpartida(){
+      return "hola";
+    }
+    public function modificarpartida(){
+      return "hols";
+    }
+    public function eliminarpartida(){
+      return "hola";
+    }
+    public function mostrarpartida(){
+      return "hola";
+    }
 }
