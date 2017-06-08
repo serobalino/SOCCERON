dd39800cf50e73d507beeab332dd1c317312ed74
dpancho349 <diegopanchomales@gmail.com>
2017-06-08 00:12:53 -0500

    partidos

---
+++ app/Partidos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Partidos extends Model
+{
+  use Notifiable;
+    protected $table = 'partidos';
+    protected $primaryKey ='id_part';
+
+}
